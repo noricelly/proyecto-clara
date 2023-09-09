effb446ec143d27b92ef6e8678469ce4b1451767
DaniaAlvarado <dania26alvarado@gmail.com>
2023-09-09 15:21:04 -0500

    links

--- src/hamburger/Hamburger.jsx
+++ src/hamburger/Hamburger.jsx
@@ -1,26 +1,51 @@
 import React, { useState } from "react";
 import './hamburger.scss';
+import Swal from "sweetalert2";
 
-const Hamburger = () => {
-    const[openMenu, setOpenMenu] = useState (false)
-    const handleMenu = ()=>{
-        if (openMenu ==false) {
+const Hamburger = async () => {
+
+    const { value: formValues } = await Swal.fire({
+        title: 'Multiple inputs',
+        html:
+          '<input id="swal-input1" class="swal2-input">' +
+          '<input id="swal-input2" class="swal2-input">',
+        focusConfirm: false,
+        preConfirm: () => {
+          return [
+            document.getElementById('swal-input1').value,
+            document.getElementById('swal-input2').value
+          ]
+        }
+      })
+      
+      if (formValues) {
+        Swal.fire(JSON.stringify(formValues))
+      }
+
+
+    const [openMenu, setOpenMenu] = useState(false)
+    const handleMenu = () => {
+        if (openMenu == false) {
             setOpenMenu(true)
         }
         else {
             setOpenMenu(false)
         }
     }
-    return(
+    return (
         <>
-        <img src='/Images/Register/hamburger.jpeg' alt="rhery" onClick={handleMenu} />
-        {
-            openMenu && 
-            <section>
-            <h1>hola</h1>
-            </section>
-        }
-            
+            <img src='/Images/Register/hamburger.jpeg' alt="rhery" onClick={handleMenu} className="hamburger" />
+            {
+                openMenu &&
+                <section className="menu">
+                     <a href="https://calendly.com/nory1291">Agende su cita</a>
+                    <a href="https://wa.me/+573043668692/">Botón de pánico</a>
+                    <span>¿Quieres ayudarnos?</span>
+                    <button>Cerrar sesión</button>
+                    
+                </section>
+            }
+
         </>
     )
 }
