78f2106c11a77b10935d1bac17a13ea8d4096c00
sebasruiz15@gmail.com <sebasruiz15@gmail.com>
2023-09-09 15:23:52 -0500

    First build landingPage

--- src/pages/landingPage/LandingPage.jsx
+++ src/pages/landingPage/LandingPage.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
-
+import React from "react";
+import { useEffect, useState } from 'react'
+import "./main.scss";
+import { useNavigate, useParams } from "react-router"
 const LandingPage = () => {
-  return (
-    <div>HOLAAAAAAAAAAAA</div>
-  )
-}
+  const navigate = useNavigate()
 
-export default LandingPage
+  const handleLogin = ()=>{
+    navigate("/login")
+  }
+  const handleRegister = ()=>{
+    navigate("/register")
+  }
+
+  return (
+    <section className="landingPage">
+      <header className="landingPage__header">
+        <button className="landingPage__header__login" onClick={handleLogin}>Ingresar</button>
+        <button className="landingPage__header__register" onClick={handleRegister}>Registrarte</button>
+      </header>
+      <figure className="landingPage__banner">
+        <img src="/Images/Aboutus/banner.svg" alt="banner" />
+      </figure>
+      <main className="landingPage__main">
+        <section className="landingPage__main__description">
+          <p>
+            CuiCare es una plataforma centrada en el apoyo a la salud mental de
+            poblaciones rurales y urbanas. Nos importa tu cuidado, quien eres y
+            como estas. Brindamos diferentes servicios de atencion y multiples
+            recursos para el mejor cuidado de salud mental.
+          </p>
+          <h2>¿Te gustaria empezar?</h2>
+          <button onClick={handleRegister}>Empieza ya</button>
+        </section>
+        <section className="landingPage__main__recognition">
+          <h2>Conecta con tu mundo interior</h2>
+          <p>Ejercicio Mindfulness</p>
+        </section>
+        <section className="landingPage__main__healthy">
+          <h3>¿Sentiste alguno de los siguientes sintomas?</h3>
+          <article>
+            <h4>¿Tienes mucho calor?</h4>
+            <figure>
+              <img src="" alt="calorExcesivo" />
+            </figure>
+          </article>
+          <article>
+            <h4>¿Tiemblas bastante?</h4>
+            <figure>
+              <img src="" alt="Temblor" />
+            </figure>
+          </article>
+        </section>
+        <section className="landingPage__main__help">
+          <h2>¿Eres profesional de la salud Mental y quieres ayudarnos?</h2>
+          <button>Brindar ayuda</button>
+        </section>
+      </main>
+    </section>
+  );
+};
+
+export default LandingPage;
