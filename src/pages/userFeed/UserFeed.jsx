222022576c398a05a56008c08f9b01c2d5003779
sebasruiz15@gmail.com <sebasruiz15@gmail.com>
2023-09-09 16:44:24 -0500

    mend

--- src/pages/userFeed/UserFeed.jsx
+++ src/pages/userFeed/UserFeed.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Hamburger from "../../hamburger/Hamburger.jsx"
+import ContentCards from '../../components/ContentCards/ContentCards.jsx'
+import Content from '../Content/Content.jsx'
 
 const UserFeed = () => {
   return (
-    <><Hamburger/></>
+    <><Hamburger/>
+    <Content/>
+    </>
   )
 }
 
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -10,7 +10,7 @@ import PublicRoute from "./PublicRoutes/PublicRoute";
 import Content from "../pages/Content/Content";
 
 const Router = () => {
-  const  userRole  = false;
+  const  userRole  = true;
 
   return (
     <BrowserRouter>
